c5591ab8548475f35904bda442e56af36b3bfd4c
Pragnesha <70869654+pragneshak23@users.noreply.github.com>
2021-06-04 09:26:18 +0530

    Add files via upload

---
+++ Calculator/Calculator/Function.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class Function
+    {
+        public static double Add(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+        public static double Sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double Multi(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double Div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public static double Mod(double num1, double num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
