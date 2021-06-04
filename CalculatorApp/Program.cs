da6d62eb9735f32b9ee380cb7922c3679c7a756e
Pragnesha <70869654+pragneshak23@users.noreply.github.com>
2021-06-04 09:24:51 +0530

    Add files via upload

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Calculator;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Function.Add(40, 10));
+            Console.WriteLine(Function.Sub(40, 10));
+            Console.WriteLine(Function.Multi(40, 10));
+            Console.WriteLine(Function.Sub(40, 10));
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
